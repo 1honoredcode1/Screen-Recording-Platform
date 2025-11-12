f7af24493c1e54204cde7fd1c258bda743b14f1f
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-11-12 19:28:21 +0100

    le video cards dummies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
 import { Header } from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 
 const page = () => {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library" />
-      <h1>MemSnip</h1>
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   );
 };
---
+++ app/(root)/video/[videoId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <main className="wrapper page">VIDEO PAGE </main>;
+};
+
+export default page;
